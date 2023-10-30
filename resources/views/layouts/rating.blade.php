40c2ad0d37177f36c26a28f5dbbf87a33aee3736
Aske <asylbek_2.06@mail.ru>
2023-10-31 00:32:46 +0600

    End work day

--- resources/views/layouts/rating.blade.php
+++ resources/views/layouts/rating.blade.php
@@ -1,9 +1,9 @@
 @extends('layouts.app')
 
 @section('content')
-        <div class="relative flex items-top justify-center min-h-screen pt-10 py-4 sm:pt-0">
-            <div class="mx-auto sm:px-6 lg:px-8 cr-white w-100 mt-5">
-                Рейтинг
-            </div>
+    <div class="relative flex items-top justify-center min-h-screen pt-10 py-4 sm:pt-0">
+        <div class="mx-auto sm:px-6 lg:px-8 cr-white w-100 mt-5">
+            Рейтинг
         </div>
+    </div>
 @endsection
--- resources/views/main/create.blade.php
+++ resources/views/main/create.blade.php
@@ -1,1 +1,27 @@
-test
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <div class="row">
+            <h1 class="">Создать турнир</h1>
+            <h6 class="mt-5">Сумма ставки</h6>
+            <div class="col-4 p-0 m-1">
+                <input class="form-control" type="text" placeholder="Сумма ставки" aria-label="default input example">
+            </div>
+            <h6 class="mt-2">Прямая трансляция в TikTok</h6>
+            <div class="form-check form-switch m-1">
+                <input class="form-check-input" type="checkbox" id="flexSwitchCheckDefault">
+                <label class="form-check-label" for="flexSwitchCheckDefault">Стрим в TikTok( Большое время ожидания )</label>
+            </div>
+            <h6 class="mt-2">Количество игроков</h6>
+            <div class="form-check form-check-inline">
+                <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio1" value="option1">
+                <label class="form-check-label" for="inlineRadio1">1vs1 <img src="{{ asset('img/userCol.png') }}" alt=""></label>
+            </div>
+            <div class="form-check form-check-inline">
+                <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio2" value="option2">
+                <label class="form-check-label" for="inlineRadio2">5vs5 <img src="{{ asset('img/teamCol.png') }}" alt=""></label>
+            </div>
+        </div>
+    </div>
+@endsection
